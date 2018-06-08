217b8e5f19f8f24ced7c320ab35d33f2c7b50803
DaoyuanWang <1833063210@qq.com>
2018-06-08 13:01:20 +0800

    优化枚举

--- property-facade/src/main/java/cn/zut/facade/enums/BusinessLevelEnum.java
+++ property-facade/src/main/java/cn/zut/facade/enums/BusinessLevelEnum.java
@@ -48,9 +48,9 @@ public enum BusinessLevelEnum implements Mapping<String, String> {
     /**
      * 停车费标准
      */
-    PARKING_LIGHT_ONE_2_FOUR_HOUR("停车费-白天-地上"),
+    PARKING_LIGHT_ONE_2_FOUR_HOUR("停车费-白天-地下"),
     PARKING_LIGHT_FOUR_2_TWELVE_HOUR("停车费-白天-地上"),
-    PARKING_NIGHT_ONE_2_FOUR_HOUR("停车费-晚上-地上"),
+    PARKING_NIGHT_ONE_2_FOUR_HOUR("停车费-晚上-地下"),
     PARKING_NIGHT_FOUR_2_TWELVE_HOUR("停车费-晚上-地上"),;
 
     private final String value;
